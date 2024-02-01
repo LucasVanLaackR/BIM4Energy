ce14adc6814d5984fa771825fb0d7b80ef4f7cd8
Bim4energy <158438523+Bim4energy@users.noreply.github.com>
2024-02-01 11:43:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,21 +119,19 @@ def main():
 
     with col1:
         st.header("3D Building Visualization")
-     # ipythreejs does not support scalar bars :(
-        pv.global_theme.show_scalar_bar = False
-        st.title("A cube")
-        st.info("""Code adapted from https://docs.pyvista.org/user-guide/jupyter/pythreejs.html#scalars-support""")
-        ## Initialize a plotter object
-        plotter = pv.Plotter(window_size=[400,400])
-        ## Create a mesh with a cube 
-        mesh = pv.Cube(center=(0,0,0))
-        ## Add some scalar field associated to the mesh
-        mesh['myscalar'] = mesh.points[:, 2]*mesh.points[:, 0]
-        ## Add mesh to the plotter
-        plotter.add_mesh(mesh, scalars='myscalar', cmap='bwr', line_width=1)
-        ## Final touches
-        plotter.view_isometric()
-        plotter.background_color = 'white'
+
+        # PyVista plot setup
+        plotter = pv.Plotter(off_screen=True)
+        cube = pv.Cube(center=(0, 0, 0), x_length=4, y_length=4, z_length=4)
+        plotter.add_mesh(cube, color='cyan', show_edges=True)
+        plotter.set_background('white')
+        plotter.camera_position = 'iso'
+        
+        # Generate screenshot of the plot
+        img = plotter.screenshot()
+        
+        # Display the screenshot in Streamlit
+        st.image(img, caption='3D Cube Visualization', use_column_width=True)
 
 ## Send to streamlit
 stpyvista(plotter, key="pv_cube")
